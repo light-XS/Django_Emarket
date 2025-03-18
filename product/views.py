9aaf2d8eda91da9e3c0c6e7430078930fda6f924
unknown <mohammadnourzekra@gmail.com>
2025-03-18 18:19:18 +0300

    added pagination

--- product/views.py
+++ product/views.py
@@ -4,17 +4,26 @@ from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
 from .filtters import ProductFilter
+from rest_framework.pagination import PageNumberPagination
+# pagination
+paginator = PageNumberPagination()
+paginator.page_size = 2
+#############
 @api_view(['GET'])
 def get_all_product(request):
     products = Product.objects.all()
-    serializer = ProductSerializer(products,many=True)
-    return Response(serializer.data)
+    count = products.count()
+    queryset = paginator.paginate_queryset(products,request)
+    serializer = ProductSerializer(queryset,many=True)
+    return Response({"data":serializer.data,"count":count})
 
 @api_view(['GET'])
 def get_product_filtered(request):
     filterset = ProductFilter(request.GET,queryset=Product.objects.all().order_by('id'))
-    serializer = ProductSerializer(filterset.qs,many=True)
-    return Response(serializer.data)
+    count = filterset.qs.count()
+    queryset = paginator.paginate_queryset(filterset.qs,request)
+    serializer = ProductSerializer(queryset,many=True)
+    return Response({"data":serializer.data,"count":count})
 
 @api_view(['GET'])
 def get_one_product(request,pk):
